f31d6edf1d4c63f9742fccf5d91ac88f0ddafbb7
Pei <nuyiep@hotmail.com>
2023-09-11 23:29:54 +0800

    125.valid palindrome kept failing- why

---
+++ easy/125.valid_palindrome.cpp
@@ -0,0 +1,45 @@
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Solution 
+{
+	public:
+		bool isPalindrome(string s) 
+		{
+			int i = 0;
+			int len = s.length();
+			string after_trim;
+
+			while (i < len)
+			{
+				if (isalpha(s[i]) == 1)
+					after_trim.push_back(s[i]);
+				i++;
+			}
+			len = after_trim.length();
+			i = 0;
+			len--;
+			int end = len;
+			while (i < end)
+			{
+				if (tolower(after_trim[len]) != tolower(after_trim[i]))
+ 					return (false);
+				i++;
+				len--;
+			}
+			return (true);
+		}
+};
+
+int main()
+{
+	string s = "race a car";
+	// string s = "A man, a plan, a canal: Panama";
+	Solution A;
+
+	bool x = A.isPalindrome(s);
+	cout << boolalpha << x << endl;
+	
+}
